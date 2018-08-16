ae5b795eae56bed7fbdcaa95f2d171af69e352f5
varunmanilal <42139581+varunmanilal@users.noreply.github.com>
2018-08-16 08:36:22 -0700

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,20 @@
+//include the library file
+#include<iostream>
+using namespace std;
+//include the main function
+int main(){
+//to declare and define the variables
+int a=1;
+char b='b';
+float c=3.043;
+double d=2.67;
+bool f=1;
+//print the values in variables and its size
+cout<<"\n value in int variable "<<a<<" size of variable "<<sizeof(a);
+cout<<"\n value in char variable "<<b<<" size of variable "<<sizeof(b);
+cout<<"\n value in float variable "<<c<<" size of variable "<<sizeof(c);
+cout<<"\n value in double variable "<<d<<" size of variable "<<sizeof(d);
+cout<<"\n value in bool variable "<<f<<" size of variable "<<sizeof(f);
+return 0;
+}
+//All done
---
+++ lab3_q2.cpp
@@ -0,0 +1,37 @@
+//include the library file
+#include<iostream>
+using namespace std;
+//include the main function
+int main(){
+//The variables
+int a,b;
+int result1;
+//1.Addition
+a = 101;
+b = 21;
+result1 = a + b;
+cout << result1<<endl;
+//2.Subtraction
+a = 122 ;
+b = 22 ;
+result1 = a - b;
+cout<< result1 <<endl;
+//3.Multiply
+a=3;
+b=14;
+result1 = a*b;
+cout<< result1 <<endl;
+//4.Division
+a=48;
+b=12;
+result1 = a/b;
+cout<< result1 <<endl;
+//5.Modulus.......
+a=100;
+b=7;
+result1 = a%b;
+cout<< result1 <<endl;
+return 0;	
+}
+//All done
+
