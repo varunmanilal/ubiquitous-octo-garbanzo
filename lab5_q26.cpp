f4c4c5923bccaeb6f878394a51d8af470c1af120
varunmanilal <42139581+varunmanilal@users.noreply.github.com>
2018-09-02 08:47:21 -0400

    Add files via upload

---
+++ lab5_q26.cpp
@@ -0,0 +1,16 @@
+//include the library file
+#include<iostream>
+using namespace std;
+int main(){
+//the varaiable is given
+int a;
+a=1;
+//the statement is printed
+cout<<"All the even numbers will be printed."<<endl;
+//the instruction and result
+while (a<=100){
+cout<<a<<endl;
+a=a+2;
+}
+return 0;
+}
---
+++ lab5_q27.cpp
@@ -0,0 +1,18 @@
+//The library file is obtained 
+#include<iostream>
+using namespace std;
+int main(){
+//The variables are given
+int a,n,b;
+a=0;
+cout<<"type the natural number you like"<<endl;
+cin>>n;
+while (a<=n){
+//The instruction and result
+cout<<"the sum of numbers "<<b<<endl;
+a++;
+b+=a;
+}
+return 0;
+}
+
