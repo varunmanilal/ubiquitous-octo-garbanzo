0c1c1463c3eb3357c64a3a81fce69baed208db6b
varunmanilal <42139581+varunmanilal@users.noreply.github.com>
2018-09-24 12:52:39 +0530

    Update bcc.cpp

--- bcc.cpp
+++ bcc.cpp
@@ -1,10 +1,7 @@
 //Tax Rate Calculation using Functions 
 #include<iostream>
 using namespace std;
-// ask user to enter the values
-// call the functions
-// number as arguement 
-// tell the  tax value to user
+
 //Write a function that obtains from the user and returns a value for unitCst, units, and taxRt to the calling module. 
 int ask (int &a, int &b,int &c)
  	 	{
@@ -31,7 +28,7 @@ int ask (int &a, int &b,int &c)
 	 	 { 
 	 	    cout<< "The unit cost over the object is " <<b<< ". "<<"The number of units are "<<a<<". "<<"The tax rate is "<<c<<". "<<"the sales tax is "<<(a*b*c)<<". "<<"The total is "<<(a*b*c) + (a*b)<<endl; 
 	 	 }
-//Write a main function with variables called unitCost, unitsPurch, taxRate, salesTax, and totalDue.
+//Write the main function with variables called unitCost, unitsPurch, taxRate, salesTax, and totalDue.
 //driver function
 	    int main ()
 	 	{
